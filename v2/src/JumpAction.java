9d5b1b5f4113b4c2bdda192b6382902e359832ef
JacobArmstrong <jarmstrong1695@gmail.com>
2016-04-26 12:45:34 -0400

    Removed unused variable from JumpAction

--- v2/src/JumpAction.java
+++ v2/src/JumpAction.java
@@ -3,16 +3,13 @@ import javafx.scene.image.Image;
 import java.util.Random;
 
 public class JumpAction extends PawnDecorator {
-
-	private int yAcceleration = 3;
 	public int count;
 	public Random randomGenerator = new Random();
 	int n = 300;
 	int z = randomGenerator.nextInt(700 - 300)+n;
-	public JumpAction(Pawn decoratedShape, int speed) {
+	public JumpAction(Pawn decoratedShape) {
 
 		super(decoratedShape);
-		this.yAcceleration = speed;
 		count = 0;
 	}
 
